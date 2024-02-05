e0e9a0f688f7e0af45462baf0edf44e83fb6bf06
Floony7 <fred.lunjevich@stretchsense.com>
2024-02-05 10:24:12 +0000

    Form and input state completed and working

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,42 @@ import { CommissionCalculator } from './components/commission-calculator.compone
 import { CommissionChart } from './components/commission-chart.component';
 
 function App() {
+  const [inputAmount, setInputAmount] = useState('');
   const [revenue, setRevenue] = useState(0);
+  // const revenueInputRef = useRef<HTMLInputElement>(null);
 
+  const handleSubmitRevenue = (e: React.FormEvent) => {
+    e.preventDefault();
+    const amount = parseInt(inputAmount);
+    // Validation: for now just log error, however, would use Zod or similar to parse input and provide user feedback
+    if (inputAmount === '' || isNaN(amount)) {
+      console.warn('Invalid amount');
+      return;
+    }
+    // Calculate and update state object for each commission band
+    setRevenue(amount);
+
+    // Clear input
+    setInputAmount('');
+  };
+  console.log('REVENUE', revenue, inputAmount);
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmitRevenue}>
         <label style={{ paddingRight: '0.5rem' }}>Enter Revenue Amount</label>
         <input
           type="text"
           name="revenue"
+          pattern="[0-9]*"
+          inputMode="numeric"
           placeholder="Enter revenue amount"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setRevenue(parseInt(e.target.value))
-          }
+          value={inputAmount}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputAmount(e.target.value)}
           style={{ padding: '5px 10px', border: '1px solid #ccc', borderRadius: '5px' }}
         />
+        <button type="submit" style={{ marginLeft: '0.5rem' }}>
+          Submit
+        </button>
       </form>
       <CommissionCalculator revenue={revenue} />
       <CommissionChart />
--- src/components/commission-calculator.component.tsx
+++ src/components/commission-calculator.component.tsx
@@ -2,28 +2,15 @@
 import { useEffect, useState } from 'react';
 import { COMMISSION_BANDS, COMMISSION_RATES } from '../utils/constants';
 
+// Pass calculated values to this component
 export const CommissionCalculator = ({ revenue }: { revenue: number }): JSX.Element => {
-  const commissionBreakdown = useCommissionCalculator(revenue);
   return (
     <section>
       <p>
-        Here will bring in the revenue of {revenue} and use the custom hook to run the calculations
+        Here will bring in the revenue of {isNaN(revenue) ? 0 : revenue} and use the custom hook to
+        run the calculations
       </p>
       This will render the chart element.
     </section>
   );
 };
-
-// type Breakdown = { band: number; commision: number };
-
-function useCommissionCalculator(revenue: number) {
-  // const [breakdownPerBand, setBreakDownPerBand] = useState<Breakdown>();
-  const [total, setTotal] = useState(0);
-
-  useEffect(() => {
-    const calculation = revenue * COMMISSION_BANDS[2];
-    setTotal(calculation);
-  }, [revenue]);
-
-  return total;
-}
